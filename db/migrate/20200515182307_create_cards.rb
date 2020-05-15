79edfe26a2ead156421eedd71e899a4e0a4356f4
nk2303 <ngankhong03@gmail.com>
2020-05-15 11:41:34 -0700

    generate resources and routes with API V1, adding foreign key

--- db/migrate/20200515182307_create_cards.rb
+++ db/migrate/20200515182307_create_cards.rb
@@ -2,6 +2,7 @@ class CreateCards < ActiveRecord::Migration[6.0]
   def change
     create_table :cards do |t|
       t.string :content
+      t.references :board, foreign_key: true
 
       t.timestamps
     end
