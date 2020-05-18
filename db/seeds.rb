ae296cfdb49fefea8b4a5dac120b87c138299b56
nk2303 <ngankhong03@gmail.com>
2020-05-17 17:03:30 -0700

    adding icon and project tables and update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# david = User.create(username: "david", full_name: "david perekopskiy", favorite_color: "blue", email: "david.perekopskiy@go.shoreline.edu", password: "123")
+david = User.create(username: "david", full_name: "david perekopskiy", favorite_color: "blue", email: "david.perekopskiy@go.shoreline.edu", password: "123")
 
-# board = Board.create(title: "favorite", image: "http://www.personal.psu.edu/amz5182/Hobbies/Graphics/hobbies.png", user_id: david.id)
-# card = Card.create(content: "Playing with little dogs are my favorite, dont forget to take out ms. kims dogs every sunday evening.", board_id: board.id)
-# card1 = Card.create(content: "Go crawfishing when you have time on the weekends.", board_id: board.id)
+project = Project.create(user_id: david.id, background_image: "", topic: "Fun Project")
+board = Board.create(title: "favorite", image: "http://www.personal.psu.edu/amz5182/Hobbies/Graphics/hobbies.png", project_id: project.id)
+card = Card.create(content: "Playing with little dogs are my favorite, dont forget to take out ms. kims dogs every sunday evening.", board_id: board.id)
+card1 = Card.create(content: "Go crawfishing when you have time on the weekends.", board_id: board.id)
