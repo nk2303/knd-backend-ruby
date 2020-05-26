5513056ca48696a72204133ad1af16ff7ab65c94
davidka7 <david.perekopskiy@go.shoreline.edu>
2020-05-25 21:05:19 -0700

    update

--- app/serializers/board_serializer.rb
+++ app/serializers/board_serializer.rb
@@ -1,5 +1,5 @@
 class BoardSerializer < ActiveModel::Serializer
-  attributes :id, :title, :image, :project_id, :cards
+  attributes :id, :title, :image, :project_id, :card_title, :cards
 
   def cards
     ActiveModel::SerializableResource.new(self.object.cards, each_serializer: CardSerializer)
