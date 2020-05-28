d59ca6adbc081fa93176dddf53e2c0fe38989c17
davidka7 <david.perekopskiy@go.shoreline.edu>
2020-05-28 13:52:59 -0700

    auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_many :projects
-
+  
+    validates :username, uniqueness: { case_sensitive: false }
     has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
       resources :users
       resources :icons
       resources :projects
+      post '/login', to: 'auth#create'
+      get '/current_user', to: 'auth#show'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
