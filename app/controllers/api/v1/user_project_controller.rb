70c03cd3b6f581c5f1db35816dbb6ee5f2521cb3
davidka7 <david.perekopskiy@go.shoreline.edu>
2020-06-07 21:38:57 -0700

    update

--- app/controllers/api/v1/user_project_controller.rb
+++ app/controllers/api/v1/user_project_controller.rb
@@ -1,2 +1,30 @@
 class Api::V1::UserProjectController < ApplicationController
+
+
+    def index
+        @user_projects = UserProject.all
+        render json: @user_projects, status: :ok
+    end
+
+    def create
+        user_project = User.create(user_project_params)
+        render json: {user_project: UserProjectSerializer.new(user_project)}
+    end
+
+    def destroy
+        user_project = UserProject.find(params[:id])
+        user_project.destroy
+        render json: {message: "Admin Deleted"}
+    end
+
+    private
+
+    def user_project_params
+        params.require(:user_project).permit(:project_id, :user_id)
+    end
+
+
+
+
+
 end
--- app/serializers/user_project_serializer.rb
+++ app/serializers/user_project_serializer.rb
@@ -1,3 +1,3 @@
 class UserProjectSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :user_id, :project_id
 end
