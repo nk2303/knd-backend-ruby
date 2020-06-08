7fde6b1df6ed777f3036125ca61cba753576b347
davidka7 <david.perekopskiy@go.shoreline.edu>
2020-06-07 21:20:14 -0700

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_034022) do
+ActiveRecord::Schema.define(version: 2020_06_08_041707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,15 @@ ActiveRecord::Schema.define(version: 2020_05_26_034022) do
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
+  create_table "user_projects", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "project_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["project_id"], name: "index_user_projects_on_project_id"
+    t.index ["user_id"], name: "index_user_projects_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "full_name"
@@ -63,4 +72,6 @@ ActiveRecord::Schema.define(version: 2020_05_26_034022) do
   add_foreign_key "boards", "projects"
   add_foreign_key "cards", "boards"
   add_foreign_key "projects", "users"
+  add_foreign_key "user_projects", "projects"
+  add_foreign_key "user_projects", "users"
 end
