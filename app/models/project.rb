a85deee607a4f89bb80cc2dcfb7025d87b1a2932
nk2303 <ngankhong03@gmail.com>
2020-06-09 17:51:50 -0700

    add an s

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
     has_many :boards
-    has_many :user_project
+    has_many :user_projects
     # has_many :users, through: :user_project
     belongs_to :user
     accepts_nested_attributes_for :boards, allow_destroy: true
