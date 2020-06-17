7d2d55f19af2de434a7892bfb8fbd7407c0281ec
davidka7 <david.perekopskiy@go.shoreline.edu>
2020-06-16 23:18:58 -0700

    s

--- db/migrate/20200515182307_create_cards.rb
+++ db/migrate/20200515182307_create_cards.rb
@@ -2,7 +2,7 @@ class CreateCards < ActiveRecord::Migration[6.0]
   def change
     create_table :cards do |t|
       t.string :content
-      t.references :board, foreign_key: true, null: false
+      t.references :board, foreign_key: true, null: false, on_delete: :cascade
     
 
       t.timestamps
