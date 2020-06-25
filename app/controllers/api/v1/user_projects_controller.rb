9534838c8c663d5051dc1d51a70451dfd09d3e36
Ngan Kim Khong <43501354+nk2303@users.noreply.github.com>
2020-06-24 18:38:55 -0700

    prevent duplicated record in user_project in progress (#5)

--- app/controllers/api/v1/user_projects_controller.rb
+++ app/controllers/api/v1/user_projects_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::UserProjectsController < ApplicationController
 
 
     def index
-        puts "IT GET TO THE WRONG ONE"
         @user_projects = UserProject.where(user_id: current_user.id )
         @project = @user_projects.map { |obj| obj }
         @project_ids = @user_projects.map { |obj| obj.project_id }
@@ -12,10 +11,28 @@ class Api::V1::UserProjectsController < ApplicationController
     end
 
     def findProjectsFromSearchdUser
-        puts "IT GETS TO THE RIGHT ONE"
         projects = UserProject.all.filter{|u_p| u_p.user_id == params[:searchUserId].to_i}
         render json: projects
     end
+
+    # def create
+    #     puts 'LOOK HERE'
+    #     puts user_project_params
+    #     target = UserProject.find{|u_p| u_p.user_id == current_user.id.to_i && u_p.project_id == user_project_params.project_id.to_i }
+    #     puts target
+    #     if target
+    #         user_project = UserProject.find(target[:id])
+    #         user_project.update(user_project_params)
+    #         if user_project.valid? 
+    #             render json: { user_project: UserProjectSerializer.new(user_project) }
+    #         else
+    #             render json: { error: 'failed to update project' }, status: :not_acceptable
+    #         end
+    #     else
+    #         user_project = UserProject.create(user_project_params)
+    #         render json: {user_project: UserProjectSerializer.new(user_project)}
+    #     end
+    # end
 
     def create
         user_project = UserProject.create(user_project_params)
@@ -33,9 +50,6 @@ class Api::V1::UserProjectsController < ApplicationController
     def user_project_params
         params.require(:user_project).permit(:project_id, :user_id, :admin_permission)
     end
-
-
-
 
 
 end
