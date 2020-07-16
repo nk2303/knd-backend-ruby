25cd13b1d0fe21bf5b42f1492124bb9c000d4e72
nk2303 <ngankhong03@gmail.com>
2020-07-16 11:49:42 -0700

    fix bugs

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,6 @@
 class Project < ApplicationRecord
     has_many :boards, dependent: :destroy
     has_many :user_projects, dependent: :delete_all
-    # has_many :users, through: :user_project
     belongs_to :user
     accepts_nested_attributes_for :boards, allow_destroy: true
     accepts_nested_attributes_for :user_projects, allow_destroy: true
